da4552aad363df27b8c7adc9ad8081a08c3cd2f2
Nat <nathalienichole.ubalde@cvsu.edu.ph>
2026-04-23 00:00:12 +0800

    final

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Home - Laboratory</title>
+
+<style>
+body {
+    font-family: "Poppins", sans-serif;
+    background: #0f172a;
+    color: #f5f7fa;
+    margin: 0;
+    padding: 0;
+    text-align: center;
+}
+
+
+nav {
+    width: fit-content;
+    margin: 15px auto 30px auto;
+    padding: 10px 25px;
+    background: rgba(255, 255, 255, 0.1);
+    border-radius: 50px;
+    backdrop-filter: blur(15px);
+    display: flex;
+    gap: 20px;
+}
+
+
+nav a {
+    color: white;
+    text-decoration: none;
+    font-weight: bold;
+    padding: 8px 15px;
+    border-radius: 20px;
+    transition: 0.3s;
+}
+
+nav a:hover {
+    background-color: #f1a0af;
+}
+
+
+h1 {
+    margin-top: 100px;
+    font-size: 40px;
+    color: #a59bca;
+}
+
+
+p {
+    font-size: 18px;
+    color: #ffffff;
+}
+</style>
+
+</head>
+<body>
+
+<nav>
+    <a href="home.php">Home</a>
+    <a href="lab1.php">Lab 1</a> 
+    <a href="lab2.php">Lab 2</a> 
+    <a href="lab3.php">Lab 3</a>
+</nav>
+
+<h1>This is my Laboratory</h1>
+<h3>Ubalde, Nathalie Nichole P. <br> BSIT 2A <br></h3>
+<p>Welcome! Click the navigation bar to view my lab activities.</p>
+
+</body>
+</html>
